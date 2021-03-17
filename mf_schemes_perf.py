498d9fc37975bb8dc876248e8a72f0742158cfd3
r2d2 devops <r2d2.devops@gmail.com>
2021-03-18 00:28:49 +0530

    added excepation handling and choices for local testing

--- mf_schemes_perf.py
+++ mf_schemes_perf.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env /Users/vkamlesh/.virtualenvs/finpy/bin/python
-#import sys
-#sys.path.insert(1, '/Users/vkamlesh/src/finpy/mftool/mftool')
 import json
 import pandas as pd
 import os.path
@@ -9,6 +7,7 @@ import sys
 
 mf = Mftool()
 #debt_fund = mf.get_open_ended_debt_scheme_performance(as_json=False)
+file='/Users/vkamlesh/src/finpy/scheme_code.json'
 
 debt_fund_keys = ['Long Duration', 
           'Medium to Long Duration', 
@@ -30,10 +29,7 @@ debt_fund_keys = ['Long Duration',
 
 
 equity_fund_keys = ['Large Cap', 'Large & Mid Cap', 'Multi Cap', 'Mid Cap', 'Small Cap', 'Value', 'ELSS', 'Contra', 'Dividend Yield', 'Focused']  
-
-#KeyError: 'Dynamic Asset Allocation or Balanced Advantage'
-#hybrid_fund_keys=['Aggressive Hybrid','Balanced Hybrid', 'Conservative Hybrid', 'Equity Savings', 'Arbitrage', 'Multi Asset Allocation', 'Dynamic Asset Allocation or Balanced Advantage']
-hybrid_fund_keys = ['Aggressive Hybrid', 'Balanced Hybrid', 'Conservative Hybrid', 'Equity Savings', 'Arbitrage', 'Multi Asset Allocation']
+hybrid_fund_keys=['Aggressive Hybrid','Balanced Hybrid', 'Conservative Hybrid', 'Equity Savings', 'Arbitrage', 'Multi Asset Allocation', 'Dynamic Asset Allocation or Balanced Advantage']
 solution_fund_keys = ["Children's","Retirement"]
 
 other_fund_keys = ["Index Funds/ETFs","FoFs(Oversease/Domestic)"]
@@ -44,81 +40,123 @@ if os.path.isfile('scheme_code.json'):
 else:
     scheme_code = mf.get_scheme_codes()
     with open('scheme_code.json', 'w') as outfile:
-        json.dump(scheme_code, outfile, indent=4)       
+        json.dump(scheme_code, outfile, indent=4)    
 
+
+
+def main(choice):
+    if choice == "1":
+        debt_fund_direct_perf()
+    elif choice == "2":
+        equity_fund_direct_perf()
+    elif choice == "3":
+        hybrid_fund_direct_perf()
+    elif choice == "4":
+        solution_fund_direct_perf()
+    elif choice == "5":
+        other_fund_direct_perf()
+    else:
+        print("Please select right choice\n")                    
+                       
+
+#Exception handling due to data unavailability at www.amfiindia.com.(https://www.amfiindia.com/research-information/other-data/mf-scheme-performance-details)
         
 def debt_fund_direct_perf():
     debt_ld = mf.get_open_ended_debt_scheme_performance()
     #writer = pd.ExcelWriter('Debt_Fund_Direct_Performance.xlsx', engine='xlsxwriter')
     with pd.ExcelWriter('Debt_Fund_Direct_Performance.xlsx') as writer:                
         for key in debt_fund_keys:
-            debt_fund_list = pd.DataFrame.from_dict(debt_ld[key])
-            debt_direct_perf = debt_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
-                                                        
-            if key == "Gilt with 10 year constant duration":
-                new_key="Gilt With 10Year"
-                debt_direct_perf.to_excel(writer, sheet_name=new_key)
-            else:    
-                debt_direct_perf.to_excel(writer, sheet_name=key)
+            try:
+                debt_fund_list = pd.DataFrame.from_dict(debt_ld[key])
+                debt_direct_perf = debt_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
+                                                            
+                if key == "Gilt with 10 year constant duration":
+                    new_key="Gilt With 10Year"
+                    debt_direct_perf.to_excel(writer, sheet_name=new_key)
+                else:    
+                    debt_direct_perf.to_excel(writer, sheet_name=key)
+            except KeyError:
+                print("Data for {} is not available Today.".format(key))
+                pass
+            except Exception as exc:
+                print("Unexpected error: {}".format(exc))         
 
 
 def equity_fund_direct_perf():
     equity_ld = mf.get_open_ended_equity_scheme_performance()
     with pd.ExcelWriter('Equity_Fund_Direct_Performance.xlsx') as writer:
         for key in equity_fund_keys:
-            equity_fund_list = pd.DataFrame.from_dict(equity_ld[key])
-            equity_direct_perf = equity_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)      
-            equity_direct_perf.to_excel(writer, sheet_name=key)
+            try:                    
+                equity_fund_list = pd.DataFrame.from_dict(equity_ld[key])
+                equity_direct_perf = equity_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)      
+                equity_direct_perf.to_excel(writer, sheet_name=key)
+            except KeyError:
+                print("Data for {} is not available Today.".format(key))
+                pass
+            except Exception as exc:
+                print("Unexpected error: {}".format(exc))     
+                
 
 def hybrid_fund_direct_perf():
     hybrid_ld = mf.get_open_ended_hybrid_scheme_performance()
-    with pd.ExcelWriter('Hybrid_Fund_Direct_Performance.xlsx') as writer:
+    with pd.ExcelWriter('Hybrid_Fund_Direct_Performance.xlsx') as writer:             
         for key in hybrid_fund_keys:
-            hybrid_fund_list = pd.DataFrame.from_dict(hybrid_ld[key])
-            hybrid_direct_perf = hybrid_fund_list.drop(columns=['latest NAV- Regular', '1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
-            hybrid_direct_perf.to_excel(writer, sheet_name=key)
-            #hybrid_fund_list.to_excel(writer,sheet_name=key)
+            try:
+                hybrid_fund_list = pd.DataFrame.from_dict(hybrid_ld[key])
+                hybrid_direct_perf = hybrid_fund_list.drop(columns=['latest NAV- Regular', '1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
+                hybrid_direct_perf.to_excel(writer, sheet_name=key)
+                #hybrid_fund_list.to_excel(writer,sheet_name=key)
+            except KeyError:
+                print("Data for {} is not available Today.".format(key))
+                pass
+            except Exception as exc:
+                print("Unexpected error: {}".format(exc)) 
 
 
-# def solution_fund_direct_perf():
-#     solution_ld = mf.get_open_ended_soultion_scheme_performance()
-#     with pd.ExcelWriter('Solution_Fund_Direct_Performance.xlsx') as writer:
-#         for key in solution_fund_keys:
-#             solution_fund_list = pd.DataFrame.from_dict(solution_ld[key])
-#             solution_direct_perf = solution_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
-#             solution_direct_perf.to_excel(writer, sheet_name=key)
 
-#def other_fund_direct_perf():
-#     other_ld = mf.get_open_ended_other_scheme_performance()
-#     with pd.ExcelFile('Other_Fund_Direct_Performance.xlsx') as writer:
-#         for key in other_fund_keys:
-#             other_fund_list = pd.DataFrame.from_dict(other_ld[key])
-#             other_direct_perf = other_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
-#             other_direct_perf.to_excel(writer,sheet_name=key)
+def solution_fund_direct_perf():
+    solution_ld = mf.get_open_ended_solution_scheme_performance()
+    with pd.ExcelWriter('Solution_Fund_Direct_Performance.xlsx') as writer: 
+        for key in solution_fund_keys:
+            try:
+                solution_fund_list = pd.DataFrame.from_dict(solution_ld[key])
+                solution_direct_perf = solution_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
+                solution_direct_perf.to_excel(writer, sheet_name=key)
+            except KeyError:
+                print("Data for {} is not available Today.".format(key))
+                pass
+            except Exception as exc:
+                print("Unexpected error: {}".format(exc))
+
+
+def other_fund_direct_perf():
+    other_ld = mf.get_open_ended_other_scheme_performance()
+    with pd.ExcelWriter('Other_Fund_Direct_Performance.xlsx') as writer: 
+        for key in other_fund_keys:
+            try:            
+                other_fund_list = pd.DataFrame.from_dict(other_ld[key])
+                other_direct_perf = other_fund_list.drop(columns=['latest NAV- Regular','1-Year Return(%)- Regular','3-Year Return(%)- Regular','5-Year Return(%)- Regular'],axis=1)
+                new_key = ''.join(s for s in key if s.isalnum) #Handle Error: Invalid Excel character '[]:*?/\' in sheetname
+                other_direct_perf.to_excel(writer,sheet_name=new_key)
+            except KeyError:
+                print("Data for {} is not available Today.".format(key))
+                pass
+            except Exception as exc:
+                print("Unexpected error: {}".format(exc))     
             
 
 
-#debt_fund_direct_perf()    
-#equity_fund_direct_perf()
-#solution_fund_direct_perf()
-#hybrid_fund_direct_perf()
-
-
-
-def mf_return(scheme_code):
-    banner="Rolling returns "
-
-    mf_name = input("Mutual Fund Name:")
-    nav_start_date = input("Investment Start Date in D-M-YYYY \n")
-    nav_end_date = input("Investment End Date in D-M-YYYY \n")
-    with open(scheme_code_file,'r') as jdata:
-        mf_code = json.load(scheme_code)
-        data = json.load(jdata)
-        for code,name in data.items():
-            if mf_name == name:
-                print("Name: {} and Code: {}".format(mf_name,code))
-
-        #value = mf.get_scheme_historical_nav_for_dates(
+# def mf_return(file):
+#     mf_name = input("Mutual Fund Name:")
+#     nav_start_date = input("Investment Start Date in D-M-YYYY \n")
+#     nav_end_date = input("Investment End Date in D-M-YYYY \n")
+#     with open(file,'r') as jdata:
+#         data = json.load(jdata)
+#         for code,name in data.items():
+#             if mf_name == name:
+#                 value = mf.get_scheme_historical_nav_for_dates(code,nav_start_date,nav_end_date)
+#                 df = pd.DataFrame(value['data'])
+#                 df.to_csv('nav_perf.csv',index=False)
 
 
 
@@ -126,7 +164,10 @@ def mf_return(scheme_code):
 
 
 
-mf_return(scheme_code)
+if __name__ == '__main__':
+
+    choice = input("Select fund category from following list.\n1.Debt\n2.Equity\n3.Hybrid\n4.Solution-Oriented\n5.Index/ETF\n")
+    main(choice)
 
 
 
